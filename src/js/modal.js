0e924d176355a9da6cd34b943087b728083531b9
Milosska <110258611+Milosska@users.noreply.github.com>
2023-06-07 19:57:34 +0300

    header modal bagfix

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,84 +1,57 @@
-!(function (e) {
-  'function' != typeof e.matches &&
-    (e.matches =
-      e.msMatchesSelector ||
-      e.mozMatchesSelector ||
-      e.webkitMatchesSelector ||
-      function (e) {
-        for (
-          var t = this,
-            o = (t.document || t.ownerDocument).querySelectorAll(e),
-            n = 0;
-          o[n] && o[n] !== t;
+const modalButtons = document.querySelectorAll('.js-open-modal');
+const overlay = document.querySelector('.js-overlay-modal');
+const closeButtons = document.querySelectorAll('.js-modal-close');
 
-        )
-          ++n;
-        return Boolean(o[n]);
-      }),
-    'function' != typeof e.closest &&
-      (e.closest = function (e) {
-        for (var t = this; t && 1 === t.nodeType; ) {
-          if (t.matches(e)) return t;
-          t = t.parentNode;
-        }
-        return null;
-      });
-})(window.Element.prototype);
+const scrollController = {
+  scrollPosition: 0,
+  disabledScroll() {
+    scrollController.scrollPosition = window.scrollY; //щоб не підскакувало вверх при закритті модалки
+    // забороняємо скрол
+    document.body.style.cssText = `
+      overflow: hidden;
+    `;
+    document.documentElement.style.scrollBehavior = 'unset';
+  },
 
-document.addEventListener('DOMContentLoaded', function () {
-  /* Записываем в переменные массив элементов-кнопок и подложку.
-      Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
-  var modalButtons = document.querySelectorAll('.js-open-modal'),
-    overlay = document.querySelector('.js-overlay-modal'),
-    closeButtons = document.querySelectorAll('.js-modal-close');
+  enabledScroll() {
+    document.body.style.cssText = ''; //----Дозволяємо скрол
+    window.scroll({ top: scrollController.scrollPosition });
+    document.documentElement.style.scrollBehavior = '';
+  },
+};
 
-  /* Перебираем массив кнопок */
-  modalButtons.forEach(function (item) {
-    /* Назначаем каждой кнопке обработчик клика */
-    item.addEventListener('click', function (e) {
-      /* Предотвращаем стандартное действие элемента. Так как кнопку разные
-            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
-            Нужно подстраховаться. */
-      e.preventDefault();
+modalButtons.forEach(btn => {
+  btn.addEventListener('click', onModalOpen);
+});
 
-      /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
-            и будем искать модальное окно с таким же атрибутом. */
-      var modalId = this.getAttribute('data-modal'),
-        modalElem = document.querySelector(
-          '.modal[data-modal="' + modalId + '"]'
-        );
+closeButtons.forEach(btn => btn.addEventListener('click', hideAll));
 
-      /* После того как нашли нужное модальное окно, добавим классы
-            подложке и окну чтобы показать их. */
-      modalElem.classList.add('active');
-      overlay.classList.add('active');
-    }); // end click
-  }); // end foreach
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    hideAll();
+    document.removeEventListener('keydown', onModalOpen);
+  }
+});
 
-  closeButtons.forEach(function (item) {
-    item.addEventListener('click', function (e) {
-      var parentModal = this.closest('.modal');
+overlay.addEventListener('click', e => {
+  if (e.target === overlay) {
+    hideAll();
+  }
+});
 
-      parentModal.classList.remove('active');
-      overlay.classList.remove('active');
-    });
-  }); // end foreach
-
-  document.body.addEventListener(
-    'keyup',
-    function (e) {
-      var key = e.keyCode;
-
-      if (key == 27) {
-        document.querySelector('.modal.active').classList.remove('active');
-        document.querySelector('.overlay').classList.remove('active');
-      }
-    },
-    false
+function onModalOpen(e) {
+  e.preventDefault();
+  scrollController.disabledScroll();
+  const modalId = this.getAttribute('data-modal');
+  const modalWindow = document.querySelector(
+    '.modal[data-modal="' + modalId + '"]'
   );
+  modalWindow.classList.add('active');
+  overlay.classList.add('active');
+}
 
-  overlay.addEventListener('click', function () {
-    document.querySelector('.modal.active').classList.remove('active');
-    this.classList.remove('active');
-  });
-}); // end ready
+function hideAll() {
+  scrollController.enabledScroll();
+  document.querySelector('.modal.active').classList.remove('active');
+  overlay.classList.remove('active');
+}
